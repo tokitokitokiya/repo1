c76bf9bd9ffaeda5b864412ced04521472343577
tokitokitokiya <tokitokitokiyahime@yahoo.co.jp>
2023-04-14 02:53:12 +0900

    ラ ラベルのPostControllerクラスの実装

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+/**
+ * Post一覧を表示する
+ * 
+ * @param Post Postモデル
+ * @return array Postモデルリスト
+ */
+
+class PostController extends Controller
+{
+    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    {
+    return $post->get();//$postの中身を戻り値にする。
+    }
+}
