ee7d7f69e2173f86869539183fd694252124fc6f
tokitokitokiya <tokitokitokiyahime@yahoo.co.jp>
2023-05-07 22:09:14 +0900

    消去できる

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+//SoftDeletesによって、SQLがDELETE文からデータが物理削除（完全削除）から論理削除（データ保持）
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     
     public function getPaginateByLimit(int $limit_count = 2){
         return $this->orderBy('updated_at','DESC')->paginate($limit_count);
